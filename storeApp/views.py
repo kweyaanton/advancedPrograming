bc53aa94a6af071e9e8850166fdc49ea60b38555
kweyaanton <kweyaanton@gmail.com>
2023-11-16 09:59:14 +0000

    additional configurations

--- storeApp/views.py
+++ storeApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request): 
+    
+    return render(request,'index.html',)
+
+
+def home(request):
+    words = request.POST['text']
+    numberOfw = len(words.split())
+    return render(request,'home.html',{'number':numberOfw})
