3af6da424a409759d9db663bbeb8db62d803a60c
kweyaanton <kweyaanton@gmail.com>
2023-11-16 16:49:41 +0000

    making login primary page

--- storeApp/views.py
+++ storeApp/views.py
@@ -6,12 +6,10 @@ from .models import Item
 # Create your views here.
 
 
+def login(request):
+    
+    return render(request,'./authentication/login.html',)
+
 def index(request):
     
     return render(request,'index.html',)
-
-
-def login(request):
-    
-
-    return render(request,'./authentication/login.html',)
