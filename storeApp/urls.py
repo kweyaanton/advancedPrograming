f475ff9fa3424758ac90b911421d160b3b61c740
kweyaanton <kweyaanton@gmail.com>
2023-11-23 17:41:47 +0000

    signup

--- storeApp/urls.py
+++ storeApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.login,name="login"),
+    path('signup',views.signup,name="signup"),
     path('index',views.index,name="index"),
+    path('checkout',views.checkout,name="checkout"),
+    
 ]
