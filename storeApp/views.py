34cf1566f0b8f933d5aa6ed8dbae7e92925ecfb8
kweyaanton <kweyaanton@gmail.com>
2023-11-23 17:41:55 +0000

    other

--- storeApp/views.py
+++ storeApp/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
-from .models import Item
 
 # Create your views here.
 
 
 def login(request):
-    
+
     return render(request,'./authentication/login.html',)
+
+def signup(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    data = {
+        'username':username,
+        'password':password,
+    }
+    
+    
+    return render(request,'./authentication/signup.html',{'data':data})
 
 def index(request):
     
     return render(request,'index.html',)
+
+def checkout(request):
+    
+    return render(request,'checkout.html',)
